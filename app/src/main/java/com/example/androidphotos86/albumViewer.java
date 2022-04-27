57f5593f844f94f314f44fd09786baea944227f5
adamfariello <adam.fariello@rutgers.edu>
2022-04-26 23:25:55 -0400

    Made the skeletons of all xml files, not all working. You have to remove bad files to test ones interested in

---
+++ app/src/main/java/com/example/androidphotos86/albumViewer.java
@@ -0,0 +1,12 @@
+package com.example.androidphotos86;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class albumViewer extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.photo_list);
+    }
+}
