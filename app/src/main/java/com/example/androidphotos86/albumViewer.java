75c5e30d46a04c810dc584832c0323ede63136df
adamfariello <adam.fariello@rutgers.edu>
2022-04-29 16:08:28 -0400

    ?2

--- app/src/main/java/com/example/androidphotos86/albumViewer.java
+++ app/src/main/java/com/example/androidphotos86/albumViewer.java
@@ -3,7 +3,6 @@ package com.example.androidphotos86;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.LinkedList;
-import java.util.stream.Stream;
 
 public class albumViewer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
